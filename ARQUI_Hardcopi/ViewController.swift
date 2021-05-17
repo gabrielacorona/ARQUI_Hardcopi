b435eb94fbd851a38bf11ff56e91e49e84451fda
gabrielacorona <gabriela.corona03@gmail.com>
2021-05-17 16:58:14 -0500

    first commit

---
+++ ARQUI_Hardcopi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARQUI_Hardcopi
+//
+//  Created by Gaby Corona on 5/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
