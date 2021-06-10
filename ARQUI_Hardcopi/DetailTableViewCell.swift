6ccad1809b28ea91022169bc6a2a82ec737ba0d2
ArelyA <arelyac01@gmail.com>
2021-06-10 06:59:15 -0500

    Updated

---
+++ ARQUI_Hardcopi/DetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DetailTableViewCell.swift
+//  ARQUI_Hardcopi
+//
+//  Created by Arturo Aceves Pérez on 10/06/21.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
