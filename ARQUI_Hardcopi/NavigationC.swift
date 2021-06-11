05bc55faacbda1852842d5eb2a441872ba36e7f3
ArelyA <arelyac01@gmail.com>
2021-06-10 21:42:53 -0500

    NO

--- ARQUI_Hardcopi/NavigationC.swift
+++ ARQUI_Hardcopi/NavigationC.swift
@@ -13,6 +13,7 @@ class NavigationC: UINavigationController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navBar.backItem?.backButtonTitle = "Atras"
         navBar.topItem?.title = "Nearby Books"
         navBar.backgroundColor = UIColor(red: 254/256, green: 244/256, blue: 233/256, alpha: 1)
         // Do any additional setup after loading the view.
